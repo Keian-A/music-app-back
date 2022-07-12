215c169b2600bc0c62f95ea12d8c34215adc1400
Keian Anthony <KeianAnthony1@gmail.com>
2022-07-12 12:13:40 -0700

    adds jwt

--- src/schema/Users.js
+++ src/schema/Users.js
@@ -6,7 +6,8 @@ const { Schema, model } = mongoose;
 const userSchema = new Schema({
     username: { type: String, required: true },
     password: { type: String, required: true },
-    email: { type: String, required: true }
+    email: { type: String, required: true },
+    token: { type: String, required: true }
 });
 
 const userModel = model('Users', userSchema);
